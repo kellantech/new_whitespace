dba341844717f777830bd8ca07f2881c062e557b
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-05-16 16:26:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ while True:
     line = file1.readline()
     if not line:
         break
-    ls.append( line)	
+    ls.append(line)
 out = ''
 for l in ls:
-	c=0
-	c2 =0
-	for n in range(len(l)):
-		if l[n] == ' ':c+=1
-	if c == 1: 		out += '+'
-	if c == 2: 		out += '-'
-	if c == 3: 		out += '.'
-	if c == 4: 		out += ','
-	if c == 5: 		out += '>'
-	if c == 5: 		out += '<'
-	if c == 6: 		out += '?'
-	if c == 7:    out += ':'
-	if c == 8:    out += ';'
-	if c == 9:    out += '#'
-	if c == 10:    out += '`'
-	if c == 11:    out += '~'
-	
-with open('out.crz','a') as f:
-	f.write(out)
+    c = 0
+    c2 = 0
+    for n in range(len(l)):
+        if l[n] == ' ': c += 1
+    if c == 1: out += '+'
+    if c == 2: out += '-'
+    if c == 3: out += '.'
+    if c == 4: out += ','
+    if c == 5: out += '>'
+    if c == 5: out += '<'
+    if c == 6: out += '?'
+    if c == 7: out += ':'
+    if c == 8: out += ';'
+    if c == 9: out += '#'
+    if c == 10: out += '`'
+    if c == 11: out += '~'
+    if c == 12: out += '['
+    if c == 13: out += ']'
+    if c >= 15 and c < 25: out += str(c - 15)
+
+with open('out.crz', 'a') as f:
+    f.write(out)
